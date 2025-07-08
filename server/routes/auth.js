878de79ce9552919d07e0e509c626b8d057c542a
kauanjsc <kauan97531@gmail.com>
2025-07-08 20:15:09 -0300

    Atualiza arquivos do back-end

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const authController = require("../controllers/authController");
 
-// Rota para cadastro de usuário
 router.post("/register", authController.register);
-
-// Rota para login
 router.post("/login", authController.login);
 
 module.exports = router;
